72859a66d50d97aebb6872c213d28e80d6a9c35d
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-01-08 23:40:06 +0200

    add RefreshMainDataWork tests

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
@@ -17,12 +17,7 @@
 package com.example.android.kotlincoroutines.main
 
 import android.content.Context
-import androidx.work.CoroutineWorker
-import androidx.work.ListenableWorker
-import androidx.work.WorkManager
-import androidx.work.Worker
-import androidx.work.WorkerFactory
-import androidx.work.WorkerParameters
+import androidx.work.*
 
 /**
  * Worker job to refresh titles from the network while the app is in the background.
@@ -41,7 +36,15 @@ class RefreshMainDataWork(context: Context, params: WorkerParameters, private va
      * start just enough to run this [Worker].
      */
     override suspend fun doWork(): Result {
-        return Result.success()         // TODO: Use coroutines from WorkManager
+        val database = getDatabase(applicationContext)
+        val repository = TitleRepository(network, database.titleDao)
+
+        return try {
+            repository.refreshTitle()
+            Result.success()
+        } catch (error: TitleRefreshError) {
+            Result.failure()
+        }
     }
 
     class Factory(val network: MainNetwork = getNetworkService()) : WorkerFactory() {
